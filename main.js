e194ec0efe05026709b60f64114c33b67e711c9b
sun573 <sun5732@gmail.com>
2019-09-10 15:04:10 -0400

    First Edit

--- main.js
+++ main.js
@@ -1,58 +1,58 @@
 $(document).ready(init);
 
 function init() {
-	console.log('Welcome to Kevin Bandy\'s web development portfolio. I just want to touch on a few technologies I have experience with and give you an overview of my skills.');
+	console.log('Welcome to Sunny Chen\'s portfolio. I just want to touch on a few technologies I have experience with and give you an overview of my skills.');
 	console.log(technologies);
 
 	technologies.forEach(function assembleTechnologiesList(technology) {
 		$('#technologies-list').append(buildTechnologyBlock(technology));
 	});
 
-	$('body').mousemove(function (e) {
-		parallax(e, $('.parallax-browser')[0], 1);
-		parallax(e, $('.parallax-browser-content')[0], 2);
-		parallax(e, $('.parallax-scatter-small')[0], .5);
-		parallax(e, $('.parallax-scatter-large')[0], 4);
-	});
+	// $('body').mousemove(function (e) {
+	// 	parallax(e, $('.parallax-browser')[0], 1);
+	// 	parallax(e, $('.parallax-browser-content')[0], 2);
+	// 	parallax(e, $('.parallax-scatter-small')[0], .5);
+	// 	parallax(e, $('.parallax-scatter-large')[0], 4);
+	// });
 
-	$('.profile-image').pressure({
-		start: function startPressure() {
-			$('.profile-image').addClass('being-pressed');
-			$('.rpm-readout').addClass('being-pressed');
-		},
-		end: function release() {
-			$('.profile-image').removeClass('being-pressed');
-			$('.rpm-readout').removeClass('being-pressed');
-			$('.tachometer-needle').css('transform', 'translateX(-50%) rotate(-90deg) scale(0.95)');
-		},
-		change: function(force, event) {
-			var angle = $.pressureMap(force, 0, 1, -90, 85);
-			var rpm = $.pressureMap(force, 0, 1, 800, 9000);
-			$('.rpm-readout').text(Math.floor(rpm) + ' RPM');
-			$('.tachometer-needle').css('transform', 'translateX(-50%) rotate(' + angle + 'deg) scale(0.95)');
-		}
-	});
+	// $('.profile-image').pressure({
+	// 	start: function startPressure() {
+	// 		$('.profile-image').addClass('being-pressed');
+	// 		$('.rpm-readout').addClass('being-pressed');
+	// 	},
+	// 	end: function release() {
+	// 		$('.profile-image').removeClass('being-pressed');
+	// 		$('.rpm-readout').removeClass('being-pressed');
+	// 		$('.tachometer-needle').css('transform', 'translateX(-50%) rotate(-90deg) scale(0.95)');
+	// 	},
+	// 	change: function(force, event) {
+	// 		var angle = $.pressureMap(force, 0, 1, -90, 85);
+	// 		var rpm = $.pressureMap(force, 0, 1, 800, 9000);
+	// 		$('.rpm-readout').text(Math.floor(rpm) + ' RPM');
+	// 		$('.tachometer-needle').css('transform', 'translateX(-50%) rotate(' + angle + 'deg) scale(0.95)');
+	// 	}
+	// });
 
-	if (window.DeviceMotionEvent) {
-		window.addEventListener('devicemotion', deviceMotionHandler);
-	}
+	// if (window.DeviceMotionEvent) {
+	// 	window.addEventListener('devicemotion', deviceMotionHandler);
+	// }
 
-	function deviceMotionHandler(e) {
-		var inputX = Math.floor(e.accelerationIncludingGravity.x * 10);
-		var inputY = Math.floor(e.accelerationIncludingGravity.y * 10 + 70);
-		$('.parallax-browser').css('transform', 'translate(' + inputX + 'px, ' + (inputY * -1) + 'px)');
-		$('.parallax-browser-content').css('transform', 'translate(' + (inputX * 2) + 'px, ' + (inputY * -2) + 'px)');
-		$('.parallax-scatter-small').css('transform', 'translate(' + (inputX * .5) + 'px, ' + (inputY * -.5) + 'px)');
-		$('.parallax-scatter-large').css('transform', 'translate(' + (inputX * 4) + 'px, ' + (inputY * -4) + 'px)');
-	}
+	// function deviceMotionHandler(e) {
+	// 	var inputX = Math.floor(e.accelerationIncludingGravity.x * 10);
+	// 	var inputY = Math.floor(e.accelerationIncludingGravity.y * 10 + 70);
+	// 	$('.parallax-browser').css('transform', 'translate(' + inputX + 'px, ' + (inputY * -1) + 'px)');
+	// 	$('.parallax-browser-content').css('transform', 'translate(' + (inputX * 2) + 'px, ' + (inputY * -2) + 'px)');
+	// 	$('.parallax-scatter-small').css('transform', 'translate(' + (inputX * .5) + 'px, ' + (inputY * -.5) + 'px)');
+	// 	$('.parallax-scatter-large').css('transform', 'translate(' + (inputX * 4) + 'px, ' + (inputY * -4) + 'px)');
+	// }
 }
 
-function parallax(e, target, multiplier) {
-	var layer_coeff = 100 / multiplier;
-	var x = (e.pageX - ($(window).width() / 2)) / layer_coeff;
-	var y = (e.pageY - ($(window).height() / 2)) / layer_coeff;
-	$(target).css('transform', 'translate(' + x + 'px,' + y + 'px)');
-}
+// function parallax(e, target, multiplier) {
+// 	var layer_coeff = 100 / multiplier;
+// 	var x = (e.pageX - ($(window).width() / 2)) / layer_coeff;
+// 	var y = (e.pageY - ($(window).height() / 2)) / layer_coeff;
+// 	$(target).css('transform', 'translate(' + x + 'px,' + y + 'px)');
+// }
 
 function buildTechnologyBlock(technology) {
 	var $outerDiv = $('<li>');
