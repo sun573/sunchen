4927f859b671aa51d54af6ac5bc9a24c19191b4e
sun573 <sun5732@gmail.com>
2019-09-10 14:31:08 -0400

    Initial test
    test with Bandy

---
+++ skills.js
@@ -0,0 +1,74 @@
+var technologies = [
+	{
+		technology: "Angular",
+		level: "Expert",
+		description: "Application architecture, building, maintaining",
+		logo: "angular",
+		primaryColor: "#C40230",
+		competence: "93"
+	},
+	{
+		technology: "SVG",
+		level: "Expert",
+		description: "Digging deep into SVG and its applications.",
+		logo: "svg",
+		primaryColor: "#FF9801",
+		competence: "98"
+	},
+	{
+		technology: "HTML / CSS",
+		level: "Expert",
+		description: "Up to date with the latest in HTML5 and CSS3 standards",
+		logo: "html5",
+		primaryColor: "#F2642A",
+		competence: "95"
+	},
+	{
+		technology: "Sass",
+		level: "Expert",
+		description: "Deep knowledge of CSS pre-processors for efficient, maintainable code",
+		logo: "sass",
+		primaryColor: "#CF6D9D",
+		competence: "99"
+	},
+	{
+		technology: "Javascript",
+		level: "Expert",
+		description: "Deep understanding of the language and how to write efficient, scalable code",
+		logo: "js",
+		primaryColor: "#EFDF5E",
+		competence: "92"
+	},
+	{
+		technology: "MySQL",
+		level: "Intermediate",
+		description: "Strengths in architecting db schema and data retrieval",
+		logo: "mysql",
+		primaryColor: "#70AFBE",
+		competence: "75"
+	},
+	{
+		technology: "PHP",
+		level: "Intermediate",
+		description: "Object oriented programming and architecture, experienced in 1.x Zend Framework",
+		logo: "php",
+		primaryColor: "#808080",
+		competence: "73"
+	},
+	{
+		technology: "React Native",
+		level: "Basic",
+		description: "Written a few non-production apps, nothing long term",
+		logo: "react_native",
+		primaryColor: "#06A5D1",
+		competence: "42"
+	},
+	{
+		technology: "Flutter",
+		level: "Basic",
+		description: "Just starting to dig in and learn this exciting offering from Google to build single codebase mobile apps!",
+		logo: "flutter",
+		primaryColor: "#42A5F5",
+		competence: "25"
+	}
+];
